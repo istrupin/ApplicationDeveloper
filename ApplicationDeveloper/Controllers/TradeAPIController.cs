bf3678f37c8e040a464afd3938ddd90657983655
unknown <istrupin@SEAL-3RCPTY1-X2.prod.ds.russell.com>
2016-07-08 13:02:18 -0700

    Created API

---
+++ ApplicationDeveloper/Controllers/TradeAPIController.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationDeveloper.Data.Repositories;
+using ApplicationDeveloper.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace ApplicationDeveloper.Controllers
+{
+    public class TradeAPIController : ApiController
+    {
+        // GET: api/TradeAPI
+        public IEnumerable<Trade> Get()
+        {
+            var repo = new TradeRepository();
+            var trades = repo.GetTrades();
+            return trades;
+        }
+
+        // GET: api/TradeAPI/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+    }
+}
--- ApplicationDeveloper/Scripts/AppDevScripts.js
+++ ApplicationDeveloper/Scripts/AppDevScripts.js
@@ -1,3 +1,23 @@
-﻿$(document).ready(function () {
-    $('.table').DataTable();
+﻿function formDate(inData) {
+    var date = new Date(inData);
+    var month = date.getMonth() + 1;
+    return (month.length > 1 ? month : "0" + month) + "/" + date.getDate() + "/" + date.getFullYear();
+}
+
+
+$(document).ready(function () {
+    $('#trades').DataTable({
+        columns: [
+            { data: "TradeDate", render: function (data) { return formDate(data)} },
+            { data: "SettleDate", render: function (data) {return formDate(data) }},
+            { data: "Symbol" },
+            { data: "Account" },
+            { data: "Quantity" },
+            { data: "Price", render: $.fn.dataTable.render.number(',', '.', 2, '$') },
+            { data: "Principal" },
+            { data: "Currency" }
+        ]
+    });
+
 });
+
